72c6466287e48cce038fb64ca1a55960d2ec39cb
Deniss Celuiko <deniss.celuiko@gmail.com>
2017-08-25 23:14:15 +0300

    Fixed merge conflict

--- app/Email.php
+++ app/Email.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
-    protected $guarded = [];
+    protected $guarded = ['created_at', 'updated_at'];
+
+    public function student() {
+        return $this->belongsTo(Student::class);
+    }
 }
