115f3c0bc5f9dbb1e012531f23a9e9122542cbfd
Deniss Celuiko <deniss.celuiko@gmail.com>
2020-08-30 23:54:15 +0300

    Closes #27

---
+++ app/Console/Commands/GenerateQRCodeArchive.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Jobs\GenerateQRCodeArchive as GeneratorJob;
+
+class GenerateQRCodeArchive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sejiens:qr-archive';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate new QR code archive for export';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $path = GeneratorJob::dispatchNow();
+        $this->info(asset('storage/'.$path));
+    }
+}
