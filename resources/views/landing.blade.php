108e7cc7ec26731c6bd7c1095f2d3dba384b273f
LIrbe <linards.irbe@inbox.lv>
2022-07-20 17:45:18 +0300

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -330,7 +330,7 @@ kuri savas zināšanas par studijām nodos tev!
         <div class="row">
             <div class="col-lg-2 text-white pr-2 text-center mt-2 mb-2"><i class="fas fa-camera fa-6x"></i></div>
             <div class="col-lg-8 mx-auto text-center">
-                <h5 class="text-white">Informējam, ka pasākuma laikā tiks fotografēts un filmēts. Apmeklējot pasākumu, Jūs piekrītat pasākuma laikā veiktajai fotografēšanai un filmēšanai, kā arī fotoattēlu un videomateriālu publiskai izmantošanai sabiedrisko attiecību vai reklāmas nolūkos LU Datorikas fakultātes un Datorikas fakultātes Studentu pašpārvaldes sociālajos medijos, tīmekļa vietnē internetā un citviet.
+                <h5 class="text-white">Informējam, ka pasākuma laikā tiks fotografēts un filmēts. Apmeklējot pasākumu, Jūs piekrītat pasākuma laikā veiktajai fotografēšanai un filmēšanai, kā arī fotoattēlu un videomateriālu publiskai izmantošanai sabiedrisko attiecību vai reklāmas nolūkos LU Datorikas fakultātes un Datorikas fakultātes Studentu pašpārvaldes sociālajos medijos, tīmekļa vietnē internetā un citviet.<br><br>Piedaloties pasākumā, Jūs piekrītat uzņemties atbildību paši par sevi. Ierodoties uz pasākumu, būs nepieciešams parakstīties, apliecinot to, ka piekrītat šiem nosacījumiem.
                 </h5>
             </div>
             <div class="col-lg-2 text-white text-center mt-2 mb-2"><i class="fas fa-video fa-6x"></i></div>
